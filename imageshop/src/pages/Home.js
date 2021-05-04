86390cc637059003f6ecc8d5bd8e48231557c10d
samarthks <samarthk0105@gmail.com>
2021-05-03 23:17:46 -0400

    add styling and api linking

--- imageshop/src/pages/Home.js
+++ imageshop/src/pages/Home.js
@@ -3,6 +3,18 @@ import ProductList from "../components/ProductList";
 
 export default function Home() {
   return (
-   <div> Home</div>
+    <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="text-center mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">
+           ImageShop
+          </h1>
+          <div className="flex mt-6 justify-center">
+            <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
+          </div>
+        </div>
+        <div className="flex flex-wrap -m-4"><ProductList /></div>
+      </div>
+    </section>
   );
 }
