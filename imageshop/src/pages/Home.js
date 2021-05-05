413137a68f1bbc37435e860ddcbd62c045ad9616
samarthks <samarthk0105@gmail.com>
2021-05-05 19:29:36 -0400

    image added

--- imageshop/src/pages/Home.js
+++ imageshop/src/pages/Home.js
@@ -10,7 +10,7 @@ render(){
       <div className="container px-5 py-24 mx-auto">
         <div className="text-center mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">
-           ImageShop
+           Movie Image Shop
           </h1>
           <div className="flex mt-6 justify-center">
             <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
